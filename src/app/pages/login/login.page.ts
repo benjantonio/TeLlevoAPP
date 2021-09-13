e8336a50467de158e6e21060c359ad79f5f99db8
Benjantonio <be.navarroa@duocuc.cl>
2021-09-13 00:40:04 -0300

    Pag inicio erronea pero sirve

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Validators, FormControl, FormControlName, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -24,14 +24,19 @@ export class LoginPage implements OnInit {
     '/ CONTRASEÑA DETECTADA=', this.contrasenaingresada);
   }
 
+
+
   ngOnInit() {
 
     this.RegisterForm = new FormGroup({
-      usuario: new FormControl('',[Validators.required, 
+      usuario: new FormControl('',[
+        Validators.required, 
         Validators.minLength(4), 
         Validators.maxLength(15),
       ]),
-      contrasena: new FormControl('',[Validators.required, 
+      contrasena: new FormControl('',[
+        Validators.required,
+        Validators.minLength(4),
       ])
       
       
