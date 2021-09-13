4d31d96899e0e1fbd1c8a1a1ae435f81e889591c
riquelmejoc <joc.riquelmem@duocuc.cl>
2021-09-13 14:02:34 -0300

    comentarios a Fran

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+//DEBES IMPORTAR LOS ROUTERS DE AQUI ARRIBA//
 
 @Component({
   selector: 'app-inicio',
@@ -8,8 +9,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class InicioPage implements OnInit {
 
+//ESTA VARIABLE RECIBE Y GUARDA EL USUARIO INGRESADO//
 usuariorecibido:any;
 
+  //AQUI INGRESAMOS LOS CONSTRUCTORES NECESARIOS PARA PODER LLAMAR A LA VARIABLE DESDE OTRA PAGE//
   constructor(private activeroute: ActivatedRoute, private router:Router) {
     this.activeroute.queryParams.subscribe(params=> {
       if(this.router.getCurrentNavigation().extras.state){
