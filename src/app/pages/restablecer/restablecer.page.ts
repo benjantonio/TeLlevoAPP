1864a0b8628b8259123cc980b165606baddd4ec4
riquelmejoc <joc.riquelmem@duocuc.cl>
2021-09-13 22:21:09 -0300

    alerta oculta

--- src/app/pages/restablecer/restablecer.page.ts
+++ src/app/pages/restablecer/restablecer.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-restablecer',
@@ -13,27 +14,42 @@ export class RestablecerPage implements OnInit {
 
   emailingresado:any;
 
-  constructor(private elrouteruwu:Router) { 
+  constructor(private elrouteruwu:Router, public alertController: AlertController) { 
   }
 
   retroceder(){
     this.elrouteruwu.navigate(['/login'])
   }
+  /*
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Revisa tu email',
+      message: 'Te hemos enviado un correo electrónico con un enlace de recuperación.',
+      buttons: ['Continuar']
+    });
 
+    await alert.present();
 
-  
-
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+  */
   ngOnInit() {
 
     this.RegisterForm = new FormGroup({
       email: new FormControl('',[
         Validators.pattern("^[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$"),
         Validators.required,
-      ]),     
-      
-      
-      
+      ]),  
+      usuario: new FormControl('',[
+        Validators.required, 
+        Validators.minLength(4), 
+        Validators.maxLength(15),
+      ]),
+               
     });
   }
+  
 
 }
