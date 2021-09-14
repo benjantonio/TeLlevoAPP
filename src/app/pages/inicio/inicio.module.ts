10ac5629f43aa9561ed3ea12ac2872173cd0bc58
FranOrtega <franc.ortegac@duocuc.cl>
2021-09-14 11:56:03 -0400

    Errores por modificar

--- src/app/pages/inicio/inicio.module.ts
+++ src/app/pages/inicio/inicio.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { InicioPageRoutingModule } from './inicio-routing.module';
-
 import { InicioPage } from './inicio.page';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+//G-MAPS
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    InicioPageRoutingModule
+    InicioPageRoutingModule,
+    MatCardModule,
+    FlexLayoutModule,
+    GooglePlaceModule
   ],
   declarations: [InicioPage]
 })
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Address } from 'cluster';
+import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
 //DEBES IMPORTAR LOS ROUTERS DE AQUI ARRIBA//
 
 @Component({
@@ -8,17 +10,28 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  title= 'angular-maps';
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild('placesRef') placesRef: GooglePlaceDirective;
+  options = {
+    types : [],
+    componentRestrictions: {country: 'CHL'}
+  };
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  title_add: any;
+  latitude:
+  longitude;
 
 //ESTA VARIABLE RECIBE Y GUARDA EL USUARIO INGRESADO//
-usuariorecibido:any;
+usuariorecibido: any;
 
   //AQUI INGRESAMOS LOS CONSTRUCTORES NECESARIOS PARA PODER LLAMAR A LA VARIABLE DESDE OTRA PAGE//
-  constructor(private activeroute: ActivatedRoute, private router:Router) {
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
     this.activeroute.queryParams.subscribe(params=> {
       if(this.router.getCurrentNavigation().extras.state){
         this.usuariorecibido= this.router.getCurrentNavigation().extras.state.usuario;
       }
-    })
+    });
    }
 
    retroceder(){
@@ -27,5 +40,11 @@ usuariorecibido:any;
 
   ngOnInit() {
   }
-
+  public handleAddressChange(address: Address) {
+    console.log(address);
+    console.log('Latitud  : ',address.geometry.location.lat());
+    console.log('Longitud  : ',address.geometry.location.lng());
 }
+}
+
+
