777ff71a44d9283c5e13ea4f9364afab98bda1c5
Benjantonio <be.navarroa@duocuc.cl>
2021-09-16 23:32:26 -0300

    cambios final

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 //DEBES IMPORTAR LOS ROUTERS DE AQUI ARRIBA//
 import { MenuController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular'; //Controlador de Toast
 
 
 @Component({
@@ -17,7 +18,7 @@ controlmenu:boolean;
 cargando: boolean;
 
   //AQUI INGRESAMOS LOS CONSTRUCTORES NECESARIOS PARA PODER LLAMAR A LA VARIABLE DESDE OTRA PAGE//
-  constructor(private activeroute: ActivatedRoute, private elrouteruwu:Router, private menu: MenuController) {
+  constructor(private activeroute: ActivatedRoute, private elrouteruwu:Router, private menu: MenuController, public toastController: ToastController) {
     this.activeroute.queryParams.subscribe(params=> {
       if(this.elrouteruwu.getCurrentNavigation().extras.state){
         this.usuariorecibido= this.elrouteruwu.getCurrentNavigation().extras.state.usuario;
@@ -42,8 +43,19 @@ cargando: boolean;
     this.cargando=false;
     await this.sleep(200);
     this.elrouteruwu.navigate(['/programar-viaje']);
-    
    }
+   
+   async buscar() {
+    const toast = await this.toastController.create({
+      message: '¡Próximamente!',
+      duration: 1500,
+      color: 'secondary',
+      position: 'bottom',
+    });
+    toast.present();
+  }
+
+
 
 
   ngOnInit() {
