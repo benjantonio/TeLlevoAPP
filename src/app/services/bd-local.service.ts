120c016e76acdc759125d0365f6c6f9b4535098d
Benjamín Navarro <be.navarroa@duocuc.cl>
2021-11-01 00:51:37 -0300

    importación elementos Storage Ionic

---
+++ src/app/services/bd-local.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BdLocalService {
+
+  private _storage: Storage | null = null;
+  constructor(private storage: Storage) { 
+    this.init();
+
+  }
+  async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+}
